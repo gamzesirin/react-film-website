b9d26d563bdfa12bf8452d1486debc9d09fefe8f
Gamze Şirin <gamze1sirin@gmail.com>
2024-01-02 18:45:00 +0300

    vercel speed test

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./App.css";
-import "./lib/fontawesome/css/all.min.css";
-import Header from "./components/Header";
-import Watched from "./components/Watched";
-import Watchlist from "./components/Watchlist";
-import Add from "./components/Add";
-import { GlobalProvider } from "./Context/GlobalState";
-import Person from "./components/Person";
+import './App.css'
+import './lib/fontawesome/css/all.min.css'
+
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+
+import Add from './components/Add'
+import { GlobalProvider } from './Context/GlobalState'
+import Header from './components/Header'
+import Person from './components/Person'
+import { SpeedInsights } from '@vercel/speed-insights/next'
+import Watched from './components/Watched'
+import Watchlist from './components/Watchlist'
 
 function App() {
-  return (
-    <GlobalProvider>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Watchlist />} />
-          <Route path="/watched" element={<Watched />} />
-          <Route path="/add" element={<Add />} />
-          <Route path="/person" element={<Person />} />
-        </Routes>
-      </Router>
-    </GlobalProvider>
-  );
+	return (
+		<GlobalProvider>
+			<SpeedInsights />
+			<Router>
+				<Header />
+				<Routes>
+					<Route path="/" element={<Watchlist />} />
+					<Route path="/watched" element={<Watched />} />
+					<Route path="/add" element={<Add />} />
+					<Route path="/person" element={<Person />} />
+				</Routes>
+			</Router>
+		</GlobalProvider>
+	)
 }
 
-export default App;
+export default App
