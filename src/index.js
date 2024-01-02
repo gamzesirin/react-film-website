f2839ddbc506d4afaeea642f34f089ac7dac7e97
Gamze Şirin <gamze1sirin@gmail.com>
2024-01-02 18:48:00 +0300

    speed test fix

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import App from './App'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
-		<SpeedInsights />
 		<App />
 	</React.StrictMode>
 )
