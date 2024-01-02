52aeccad35eff7af5ffad59c696733455909f771
Gamze Şirin <gamze1sirin@gmail.com>
2024-01-02 18:46:50 +0300

    speed test

--- src/App.js
+++ src/App.js
@@ -7,14 +7,12 @@ import Add from './components/Add'
 import { GlobalProvider } from './Context/GlobalState'
 import Header from './components/Header'
 import Person from './components/Person'
-import { SpeedInsights } from '@vercel/speed-insights/next'
 import Watched from './components/Watched'
 import Watchlist from './components/Watchlist'
 
 function App() {
 	return (
 		<GlobalProvider>
-			<SpeedInsights />
 			<Router>
 				<Header />
 				<Routes>
