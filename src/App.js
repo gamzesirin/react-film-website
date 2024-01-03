9f80b29a19af0498e5ff066e9a9d1b9bfd60ffc0
Gamze Şirin <gamze1sirin@gmail.com>
2024-01-03 11:18:04 +0300

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import './App.css'
-import './lib/fontawesome/css/all.min.css'
+import "./App.css";
+import "./lib/fontawesome/css/all.min.css";
 
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
-import Add from './components/Add'
-import { GlobalProvider } from './Context/GlobalState'
-import Header from './components/Header'
-import Person from './components/Person'
-import Watched from './components/Watched'
-import Watchlist from './components/Watchlist'
+import Add from "./components/Add";
+import { GlobalProvider } from "./Context/GlobalState";
+import Header from "./components/Header";
+import Person from "./components/Person";
+import Watched from "./components/Watched";
+import Watchlist from "./components/Watchlist";
 
 function App() {
-	return (
-		<GlobalProvider>
-			<Router>
-				<Header />
-				<Routes>
-					<Route path="/" element={<Watchlist />} />
-					<Route path="/watched" element={<Watched />} />
-					<Route path="/add" element={<Add />} />
-					<Route path="/person" element={<Person />} />
-				</Routes>
-			</Router>
-		</GlobalProvider>
-	)
+  return (
+    <GlobalProvider>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Watchlist />} />
+          <Route path="/watched" element={<Watched />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/person" element={<Person />} />
+          merhaba
+        </Routes>
+      </Router>
+    </GlobalProvider>
+  );
 }
 
-export default App
+export default App;
